ceb9176a369b3a4a2c189ab61c8156d884fb75a6
alexkozik <alex_kozik@hotmail.com>
2025-05-06 21:18:06 +0300

    add base func

---
+++ main.py
@@ -0,0 +1,18 @@
+def sum_of(a, b):
+    return a+b
+
+def sub_of(a, b):
+    return a-b
+
+def mul_of(a, b):
+    return a*b
+
+def div_of(a, b):
+    return a/b
+
+op1 = 5
+op2 = 6
+print(f"sum of {op1} and {op2} is {sum_of(op1,op2)}")
+print(f"sub of {op1} and {op2} is {sub_of(op1,op2)}")
+print(f"mul of {op1} and {op2} is {mul_of(op1,op2)}")
+print(f"div of {op1} and {op2} is {div_of(op1,op2)}")
